c1df679afb934ae19f6aa5c96226255ef873fc58
M1seryy <62029606+M1seryy@users.noreply.github.com>
2023-04-19 20:26:18 +0300

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -5,9 +5,11 @@ const symbolLength = document.querySelector(`input[data-length="6"]`);
 
 input.addEventListener("blur", (e) => {
   if (e.currentTarget.value.length == symbolLength.dataset.length) {
+    input.classList.remove("invalid")
     input.classList.add("valid");
     console.log(e.currentTarget.value, symbolLength.dataset.length);
   } else {
+    input.classList.remove("valid")
     input.classList.add("invalid");
   }
 });
