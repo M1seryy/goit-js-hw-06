9bf99a487f538d8156aefe32f06c4aa8b803b6fa
M1seryy <62029606+M1seryy@users.noreply.github.com>
2023-04-19 20:24:52 +0300

    ю

--- js/task-07.js
+++ js/task-07.js
@@ -1,6 +1,6 @@
 const text = document.getElementById("text");
 const rangeValue = document.getElementById("font-size-control");
 
-rangeValue.addEventListener("change", () => {
+rangeValue.addEventListener("input", () => {
   text.style.fontSize = rangeValue.value + "px";
 });
